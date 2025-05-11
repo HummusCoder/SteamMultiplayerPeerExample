5be1077ca21bcfb76070f119fc2c174a71016e42
HummusThing <123277633+HummusThing@users.noreply.github.com>
2025-05-11 06:32:26 -0400

    Fixed flipping player sprite when turning

--- fisher.gd
+++ fisher.gd
@@ -27,88 +27,91 @@ func set_authority(id : int) -> void:
 	set_multiplayer_authority(id)
 
 func _physics_process(delta):
-	
-	set_collision_mask_value(2, true)
-	set_collision_mask_value(3, true)
-	set_collision_mask_value(4, true)
-	
-	anim_player.play("idle")
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		if Input.is_action_just_pressed("jump") and airJumps > 0:
+	if is_multiplayer_authority():
+		set_collision_mask_value(2, true)
+		set_collision_mask_value(3, true)
+		set_collision_mask_value(4, true)
+		
+		anim_player.play("idle")
+		
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y += gravity * delta
+			if Input.is_action_just_pressed("jump") and airJumps > 0:
+					velocity.y = JUMP_VELOCITY
+					airJumps -= airJumps
+					if !Input.get_axis("left", "right"):
+						velocity.x = 0
+			if Input.is_action_pressed("down"):
+					set_collision_mask_value(2, false)
+					set_collision_mask_value(3, false)
+					set_collision_mask_value(4, false)
+			
+			#if Input.is_action_pressed("jump"):
+				#if Input.is_action_pressed("down"):
+					#set_collision_mask_value(2, false)
+					#set_collision_mask_value(3, false)
+					#set_collision_mask_value(4, false)
+				#elif Input.is_action_just_pressed("jump") and airJumps > 0:
+					#velocity.y = JUMP_VELOCITY
+					#airJumps -= airJumps
+					#if !Input.get_axis("left", "right"):
+						#velocity.x = 0
+		
+		#Handle jump
+		#if Input.is_action_just_pressed("jump") and is_on_floor():
+			#if Input.is_action_pressed("down") and canFall:
+				#set_collision_mask_value(2, false)
+				#set_collision_mask_value(3, false)
+				#set_collision_mask_value(4, false)
+			#else:
+				#velocity.y = JUMP_VELOCITY
+		if is_on_floor():
+			if Input.is_action_just_pressed("jump"):
 				velocity.y = JUMP_VELOCITY
-				airJumps -= airJumps
-				if !Input.get_axis("left", "right"):
-					velocity.x = 0
-		if Input.is_action_pressed("down"):
+			if Input.is_action_pressed("down"):
 				set_collision_mask_value(2, false)
 				set_collision_mask_value(3, false)
 				set_collision_mask_value(4, false)
 		
-		#if Input.is_action_pressed("jump"):
-			#if Input.is_action_pressed("down"):
-				#set_collision_mask_value(2, false)
-				#set_collision_mask_value(3, false)
-				#set_collision_mask_value(4, false)
-			#elif Input.is_action_just_pressed("jump") and airJumps > 0:
-				#velocity.y = JUMP_VELOCITY
-				#airJumps -= airJumps
-				#if !Input.get_axis("left", "right"):
-					#velocity.x = 0
-	
-	#Handle jump
-	#if Input.is_action_just_pressed("jump") and is_on_floor():
-		#if Input.is_action_pressed("down") and canFall:
-			#set_collision_mask_value(2, false)
-			#set_collision_mask_value(3, false)
-			#set_collision_mask_value(4, false)
-		#else:
-			#velocity.y = JUMP_VELOCITY
-	if is_on_floor():
-		if Input.is_action_just_pressed("jump"):
-			velocity.y = JUMP_VELOCITY
-		if Input.is_action_pressed("down"):
-			set_collision_mask_value(2, false)
-			set_collision_mask_value(3, false)
-			set_collision_mask_value(4, false)
-	
-	#var input_direction = Input.get_vector("left", "right", "up", "down")
-	var input_direction = Input.get_axis("left", "right")
-	
-	#if input_direction and controlling:
-	if is_on_floor():
-		if airJumps == 0:
-			airJumps = 1
-		if momentum != 0 && abs(momentum) > abs(SPEED):
-			if velocity.x * input_direction < 0:
-				momentum += input_direction * AIRSPEED
-				velocity.x = momentum
+		#var input_direction = Input.get_vector("left", "right", "up", "down")
+		var input_direction = Input.get_axis("left", "right")
+		
+		#if input_direction and controlling:
+		if is_on_floor():
+			if airJumps == 0:
+				airJumps = 1
+			if momentum != 0 && abs(momentum) > abs(SPEED):
+				if velocity.x * input_direction < 0:
+					momentum += input_direction * AIRSPEED
+					velocity.x = momentum
+				else:
+					velocity.x = momentum
 			else:
-				velocity.x = momentum
-		else:
-			velocity.x = input_direction * SPEED
-		
-		momentum = move_toward(momentum, 0, FRICTION)
-		
-		######################################
-		#flips sprite when changing directions
-		######################################
-		if ((velocity.x < 0 && facingLeft == false) || (velocity.x > 0 && facingLeft == true)):
-			sprite.scale.x *= -1
-			facingLeft = !facingLeft
-		
-	if !is_on_floor():
-		#momentum += input_direction * AIRSPEED * delta
-		if abs(velocity.x) > abs(SPEED):
-			if velocity.x * input_direction < 0:
+				velocity.x = input_direction * SPEED
+			
+			momentum = move_toward(momentum, 0, FRICTION)
+			
+			######################################
+			#flips sprite when changing directions
+			######################################
+			#if ((velocity.x < 0 && facingLeft == false) || (velocity.x > 0 && facingLeft == true)):
+				#sprite.scale.x *= -1
+				#facingLeft = !facingLeft
+			
+		if !is_on_floor():
+			#momentum += input_direction * AIRSPEED * delta
+			if abs(velocity.x) > abs(SPEED):
+				if velocity.x * input_direction < 0:
+					velocity.x += input_direction * AIRSPEED
+			else:
 				velocity.x += input_direction * AIRSPEED
-		else:
-			velocity.x += input_direction * AIRSPEED
-			#velocity.x = input_direction * SPEED
-		momentum = velocity.x
-	move_and_slide()
+				#velocity.x = input_direction * SPEED
+			momentum = velocity.x
+		move_and_slide()
+	if ((velocity.x < 0 && facingLeft == false) || (velocity.x > 0 && facingLeft == true)) && is_on_floor():
+				sprite.scale.x *= -1
+				facingLeft = !facingLeft
 
 func set_player_name(value : String):
 	$label.text = value
